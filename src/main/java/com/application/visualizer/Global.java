cafae9e389078349f98e748119cb06e79a822bbb
balaz <balazskosa3@gmail.com>
2022-04-27 17:57:00 +0200

    add global variable for styling

---
+++ src/main/java/com/application/visualizer/Global.java
@@ -0,0 +1,14 @@
+package com.application.visualizer;
+
+public class Global {
+    public static final String BASE_BACKGROUND_COLOR = "tomato";
+    public static final String SELECTED_BACKGROUND_COLOR = "#3fadd9";
+    public static final String SECOND_SELECTED_BACKGROUND_COLOR = "#a88939";
+    public static final String SORTED_BACKGROUND_COLOR = "#329456";
+    public static final String BASE_BORDER = "1px solid black";
+    public static final String SELECTED_BORDER = "2px solid white";
+    public static final String SORTED_BORDER = "3px solid #013317";
+
+
+    private Global() {}
+}
