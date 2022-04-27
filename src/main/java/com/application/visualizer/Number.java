1cd1303a21622b4eea777321395b36d80f6415c9
balaz <balazskosa3@gmail.com>
2022-04-27 18:11:58 +0200

    update Number class

--- src/main/java/com/application/visualizer/Number.java
+++ src/main/java/com/application/visualizer/Number.java
@@ -7,13 +7,11 @@ import com.vaadin.flow.component.html.Div;
 public class Number extends Div {
 
     private int value;
-    private final String baseColor;
     private Boolean isAnimated = false;
 
-    public Number(Integer value, String baseColor) {
+    public Number(Integer value) {
         setValue(value);
-        this.baseColor = baseColor;
-        setBackgroundColor(this.baseColor);
+        setBackgroundColor(Global.BASE_BACKGROUND_COLOR);
         this.addClassName("number");
     }
 
@@ -26,21 +24,41 @@ public class Number extends Div {
         this.setText(String.valueOf(value));
     }
 
-    public void setBackgroundColor(String color) {
-        this.getElement().getStyle().set("background", color);
-    }
-
-    public void resetBackgroundColor() {
-        this.getElement().getStyle().set("background", this.baseColor);
-    }
-
     public void animation() {
-        if(this.isAnimated) {
+        if (this.isAnimated) {
             this.getElement().getStyle().set("animation", "switchFirst 3s");
             this.isAnimated = false;
             return;
         }
         this.getElement().getStyle().set("animation", "switchSecond 3s");
         this.isAnimated = true;
+    }
+
+    public void setBackgroundColor(String color) {
+        this.getElement().getStyle().set("background", color);
+    }
+
+    public void setBorder(String border) {
+        this.getElement().getStyle().set("border", border);
+    }
+
+    public void resetStyle() {
+        setBackgroundColor(Global.BASE_BACKGROUND_COLOR);
+        setBorder(Global.BASE_BORDER);
+    }
+
+    public void selectedStyle() {
+        setBackgroundColor(Global.SELECTED_BACKGROUND_COLOR);
+        setBorder(Global.SELECTED_BORDER);
+    }
+
+    public void secondSelectedStyle() {
+        setBackgroundColor(Global.SECOND_SELECTED_BACKGROUND_COLOR);
+        setBorder(Global.SELECTED_BORDER);
+    }
+
+    public void sortedStyle() {
+        setBackgroundColor(Global.SORTED_BACKGROUND_COLOR);
+        setBorder(Global.SORTED_BORDER);
     }
 }
