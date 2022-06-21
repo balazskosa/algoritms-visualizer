a853d0f9c6e501fd6e4cbce2795f3eb25dcf3adc
balaz <balazskosa3@gmail.com>
2022-06-22 00:50:09 +0200

    login

--- src/main/java/com/application/User.java
+++ src/main/java/com/application/User.java
@@ -2,52 +2,27 @@ package com.application;
 
 public class User {
 
-    private String firstName;
-    private String lastName;
-    private String email;
-    private String picture;
+    private final String firstName;
+    private final String lastName;
+    private final String email;
 
-    public User(String firstName, String lastName, String email, String picture) {
+    public User(String firstName, String lastName, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
-        this.picture = picture;
     }
 
-    public String getPicture() {
-        return picture;
-    }
-
-    public void setPicture(String picture) {
-        this.picture = picture;
-    }
 
     public String getFirstName() {
         return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
     }
 
     public String getLastName() {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @Override
-    public String toString() {
-        return firstName;
-    }
 }
